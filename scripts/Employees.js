9aefac31c443eefa87f22c124da31d782556cc4b
David Profitt <dlprofitt@gmail.com>
2023-03-10 08:48:05 -0500

    Book-3_Brewed-Awakenings close enough

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,10 @@
 import { getEmployees } from "./database.js"
+import { getProducts } from "./database.js"
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
+const prodObject = getProducts()
+const orderObject = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
@@ -8,6 +12,24 @@ export const Employees = () => {
     for (const employee of employees) {
         html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
+
+    document.addEventListener(
+        "click",
+        (clickEvent) => {
+            const itemClicked = clickEvent.target
+  
+            if (itemClicked.id.startsWith("employee")) {
+                const [, employeePrimaryKey] = itemClicked.id.split("--")
+  
+                for (const employeeObject of employees) {
+                    if (employeeObject.id === parseInt(employeePrimaryKey)) {
+                            window.alert(`${employeeObject.name} has sold `)
+                        }
+                    }
+                }
+            }
+        
+    )
 
     html += "</ul>"
 
